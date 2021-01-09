9bf47dba714dbfecda91c8908d9ac066f69d9022
formarx <formarx@gmail.com>
2021-01-09 18:22:28 +0900

    google auth

--- bookmark/views.py
+++ bookmark/views.py
@@ -6,6 +6,13 @@ from django.urls import reverse_lazy
 
 from .models import *
 
+import os.path
+import pickle
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+
 # Create your views here.
 class BookmarkListView(ListView):
     model = Bookmark
@@ -28,3 +35,35 @@ class BookmarkUpdateView(UpdateView):
 class BookmarkDeleteView(DeleteView):
     model = Bookmark
     success_url = reverse_lazy('list')
+
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+def GoogleAuth():
+    creds = None
+
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+        
+    service = build('drive', 'v3', credentials=creds)
+
+    results = service.files().list(
+        pageSize=10, fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files', [])
+
+    if not items:
+        print('No files found')
+    else:
+        print('Files:')
+        for item in items:
+            print(u'{0} ({1})'.format(item['name'], item['id']))
